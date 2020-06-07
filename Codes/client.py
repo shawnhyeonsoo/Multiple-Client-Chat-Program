20d947fcc97102c3d24418127a765f547ad212c4
shawnhyeonsoo <64349347+shawnhyeonsoo@users.noreply.github.com>
2020-06-07 23:34:11 +0900

    Update client.py

--- Codes/client.py
+++ Codes/client.py
@@ -1,40 +1,29 @@
 import socket
 import argparse
 import threading
-#접속하고 싶은 ip와 port를 입력받는 클라이언트 코드를 작성해보자.
-# 접속하고 싶은 포트를 입력한다.
-port = 4000
 
-def handle_receive(lient_socket):
+def handle_receive(client_socket):
     while 1:
         try:
             data = client_socket.recv(1024)
         except:
-            print("연결 끊김")
+            print("disconnected")
             break
         data = data.decode('utf-8')
-        #if not user in data:
         print(data)
 
 def handle_send(client_socket):
     while 1:
         data = input()
         client_socket.send(data.encode('utf-8'))
-        if data == "/종료":
+        if data == "/exit":
             break
     client_socket.close()
 
 
 if __name__ == '__main__':
-    #IPv4 체계, TCP 타입 소켓 객체를 생성
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # 지정한 host와 prot를 통해 서버에 접속합니다.
     client_socket.connect(("127.0.0.1", 4000))
-
-
-    #client_socket.send(user.encode('utf-8'))
-
     receive_thread = threading.Thread(target=handle_receive, args=(client_socket,))
     receive_thread.daemon = True
     receive_thread.start()
